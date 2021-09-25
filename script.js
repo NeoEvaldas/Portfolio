195ee4210ff15887cea7d387b6a1280acfedba60
NeoEvaldas <neoevaldas@gmail.com>
2021-09-25 22:15:09 +0300

    Puslapio pridejimas
    Prideti puslapiai,
    Susieta su meniu

--- script.js
+++ script.js
@@ -6,3 +6,14 @@ toggler.addEventListener("click", () => {
   menu.classList.toggle("active");
 })
 
+function toggle(IDS) {
+  var keisti = document.getElementById('container').getElementsByTagName('div');
+  for (var i=0; i<keisti.length; i++) { 
+    if (keisti[i].id != IDS) { keisti[i].style.display = 'none'; }
+  }
+  var status = document.getElementById(IDS).style.display;
+  if (status == 'block') { document.getElementById(IDS).style.display = 'none'; }
+                    else { document.getElementById(IDS).style.display = 'block'; }
+
+  return false;
+}
